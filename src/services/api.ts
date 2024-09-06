2403ceacb34dd8b0250c9de3032f5cdd9330454a
AndreyShal <andreyshalomitsky@gmail.com>
2024-09-06 18:57:03 +0300

    "Add deployment configuration"

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import { User } from '../App.tsx';
+import { User } from '../components/UserTable/UserTable.tsx';
 
 export const fetchUsers = async (): Promise<User[]> => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
